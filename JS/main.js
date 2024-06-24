7fe438c026476d4eee44365b85621d6d9e73ec63
NicolaTeora <nikxodia@gmail.com>
2024-06-24 12:00:08 +0200

    fix : suggestions, class items

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,43 @@
 //dichiarazione variabili ed array
 let userEmail;
-const userEmailElement = document.getElementById('user_email');
-const submit = document.getElementById('submit');
-const listEmail = ['nikxo@boolean.com', 'class117@boolean.com', 'pincopallo@email.it'];
+const userEmailElement = document.getElementById("user_email");
+const submit = document.getElementById("submit");
+const listEmail = [
+  "nikxo@boolean.com",
+  "class117@boolean.com",
+  "pincopallo@email.it",
+];
 
 //creo l'evento per generare il controllo
-submit.addEventListener("click", function(){
-    userEmail = userEmailElement.value;
-    //console.log(userEmail);
+submit.addEventListener("click", function () {
+  userEmail = userEmailElement.value;
+  //console.log(userEmail);
 
-    let correct = false;
+  let correct = false;
 
-    for (i = 0; i < listEmail.length; i++){
-        if ( userEmail == listEmail[i]){
-            correct = true;            
-            console.log(correct);
-        };  
-    };
-
-    if ( correct == true) {
-        alert('Identità Confermata! Benvenuto!');    
-    } else {
-        alert('ATTENZIONE!! Accesso Negato!');
+  for (i = 0; i < listEmail.length; i++) {
+    if (userEmail == listEmail[i]) {
+      correct = true;
+      console.log(correct);
     }
+  }
 
+  if (correct == true) {
+    alert("Identità Confermata! Benvenuto!");
+    userEmailElement.value = " ";
+  } else {
+    alert("ATTENZIONE!! Accesso Negato!");
+    userEmailElement.value = " ";
+  }
 });
 
-  document.getElementById('suggestion').innerHTML = `
-    <p>${listEmail}</p>`;
+let text = "";
+for (let x of listEmail) {
+  text += x + "<br>";
+}
+
+function showSuggestions() {
+  document.getElementById("suggestion").innerHTML = text;
+}
+
+document.getElementById("buttonOne").addEventListener("click", showSuggestions);
